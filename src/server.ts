4483900027d18bd02cf0fcc199095302ad2d7b59
josue <josuealbelaez@gmail.com>
2025-02-24 09:09:28 -0300

    agregar enlaces frontend

--- src/server.ts
+++ src/server.ts
@@ -76,7 +76,9 @@ app.use(cors({
     'http://localhost:8080',
     'https://fluentphrases.org',
     'https://interlineado-backend-fluent-phrases.vercel.app',
-    'https://backend-interlineado.vercel.app'
+    'https://backend-interlineado.vercel.app',
+    'https://completointerlineadofluentphrases-c11lddowc.vercel.app/'
+    
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
